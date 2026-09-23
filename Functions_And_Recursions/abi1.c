f028c77c4ad347eac871f2bb49af6c5beb25804d
Abinash Das <iamabinashdas01@gmail.com>
2026-09-24 02:10:56 +0530

    New folder and files added

---
+++ Functions_And_Recursions/abi1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//write 2 function ie 1.print "hello" and 2.print"good bye"
+void printhello();
+void printgoodbye();
+int main(){
+    printhello();
+    printgoodbye();
+    return 0;
+}
+void printhello(){
+    printf("Hello\n");
+}
+void printgoodbye(){
+    printf("Good bye\n");
+}
